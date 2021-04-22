85ab4969a9dd65bf8c80fce60969b3e27c0c62bd
lazapatare <80972766+lazapatare@users.noreply.github.com>
2021-04-22 02:02:16 -0500

    Se ejecuta PrimerArchivo.py

--- JewelApp/PrimerArchivo.py
+++ JewelApp/PrimerArchivo.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 
 class aplicacion(App):
     def build(self):
-        return Label(text='Bienvenido a Jewel App')
+        return Label(text='Bienvenidos a Jewel App')
 
 if __name__ =='__main__':
     aplicacion().run()
