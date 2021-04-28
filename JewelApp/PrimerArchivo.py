62385ccab769998931a8e6e8ce013386d8b01b15
Laura Cañaveral Agudelo <>
2021-04-28 23:13:46 +0000

    comente el código

--- JewelApp/PrimerArchivo.py
+++ JewelApp/PrimerArchivo.py
@@ -2,10 +2,11 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.label import Label
-
+# definicion de la clase base de la aplicacion
 class aplicacion(App):
+  # esta etiqueta es la raiz widget de la aplicacion
     def build(self):
         return Label(text='Bienvenidos a Jewel App')
-
+# (run) esto inicia y comienza a ejecutar la aplicacion 
 if __name__ =='__main__':
     aplicacion().run()
